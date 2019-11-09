d1c7ef5fc1e6fccc1fe24070359c2894a8ad6dee
Matevž Špacapan <matevz.spacapan@gmail.com>
2019-11-09 11:38:37 +0100

    Security fix

--- app/loaders/signup.php
+++ app/loaders/signup.php
@@ -8,7 +8,7 @@ class SignUp extends Connection{
 		//sign up account
 		elseif(isset($_POST['sign_up_acc'])){
 			$sign_up_model=$this->loadSQL('SignUpModel');
-			$_SESSION['alert']=$sign_up_model->signupAcc($_POST['username'], filter_var($_POST['email'], FILTER_SANITIZE_EMAIL), $_POST['password'], $_POST['g-recaptcha-response'], $_POST['newsletter']);
+			$_SESSION['alert']=$sign_up_model->signupAcc(strip_tags($_POST['username']), filter_var($_POST['email'], FILTER_SANITIZE_EMAIL), strip_tags($_POST['password']), strip_tags($_POST['g-recaptcha-response']), strip_tags($_POST['newsletter']));
 			echo '<script>window.history.back();</script>';
 		}
 		else{
